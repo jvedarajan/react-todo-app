4b4c46a44a5fc47085484d3ace906effcddfa816
jvedarajan <jvedarajan@teksystems.com>
2018-06-25 19:54:28 +0530

    users chat bug dynamic component routing issue fixed..

--- src/App.js
+++ src/App.js
@@ -32,17 +32,52 @@ class App extends Component {
        }else{
               this.setState({ redirect: true,menus:{} });  
        }   */
-      const userDefaultMenus = userMenus;
-   
+      const userDefaultMenus = userMenus; 
       const userName = localStorage.getItem('loggedUserName');
       const userRowID = localStorage.getItem('loggedUserID');
       this.state = { redirect: false, menus: userDefaultMenus, userRowIndex: userRowID, allUsersInfo: "", loggedUser: loggedUserEmail, loggedUserName: userName };
-    
     } else {
       this.setState({ redirect: true, menus: {} });
     }
   }
-  
+  componentDidMount = () => {
+   this.callApigetCustomMenus(this.state.userRowIndex);
+}
+  /* get user's Custom Menus*/
+  callApigetCustomMenus = async (id) => {
+    const _this = this;
+    const response = await fetch('/api/getUserCustomMenus',
+            {
+                    method: 'POST',
+                    body: JSON.stringify({
+                            user: id
+                    }),
+                    headers: { "Content-Type": "application/json" }
+            });
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    if (body.status === "OK") {
+            const data = body.data;
+            data.map(custommenus => {
+                    const menuName = custommenus.menuname;
+                    const getStateMenus = _this.state.menus;
+                    const createJSON = {
+                            "menuname": menuName, "iconclass": "fa fa-asterisk", "added_lists": [],
+                            "display_order": custommenus.menu_display_order,
+                            "created_at": custommenus.created_at,
+                            "type": "custom",
+                            "path": custommenus.path,
+                            "component": "TasksComponent"
+                    };
+                    const key = menuName;
+                    const createMenuObj = {};
+                    createMenuObj[key] = createJSON;
+                    const newMenusAdd = Object.assign({}, getStateMenus, createMenuObj);
+                    _this.setState({ menus: newMenusAdd });
+
+            });
+    }
+}
   render() {
     const _this = this;
    // const redirect = _this.state.redirect;
